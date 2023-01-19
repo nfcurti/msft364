3552393284083ae9e8259807031aa63f5f05f9bc
nfcurti <nicolascurti75@gmail.com>
2023-01-19 21:35:25 +0100

    first

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import '@/styles/globals.css'
+import '@/styles/popOne.css'
+import '@/styles/popTwo.css'
+import '@/styles/popThree.css'
+import '@/styles/popFour.css'
+
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
